40a285b98006568da8375f9a5da97ea8cc2e33e7
pgonzalezs1999 <pgonzalezs1999@gmail.com>
2024-08-01 22:38:15 +0200

    Changed server to remote

--- android_studio/lib/environment.dart
+++ android_studio/lib/environment.dart
@@ -1,12 +1,12 @@
 class Environment {
-  static const String serverUrl = 'http://192.168.1.74:8000';
+  static const String serverUrl = 'https://api.wefoodcompany.com';
   static const String apiUrl = '$serverUrl/api/auth/';
   static const String storageUrl = '$serverUrl/storage/';
 
   static const double defaultHorizontalMargin = 0.05;
   static const double defaultVerticalMargin = 0.05;
 
-  static const String googleMapsApiKey = 'AIzaSyCD-4i7nMlqnezxdNLXQNHARS2ETYNa528';
+  static const String googleMapsApiKey = 'AIzaSyBSyMRo2cAF7qSaS8ZTA2BjoqiwKiENsHM';
   static const String supportPhone = '+51912051667';
   static const String supportEmail = 'info@wefoodcompany.com';
 
