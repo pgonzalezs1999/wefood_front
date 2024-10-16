61434573e961fe61f6b89598099e8d648b96e860
Jose Rosa <jrosamanan@gmail.com>
2024-10-16 17:36:59 -0400

    chore: add environment-specific configurations using kDebugMode

--- wefood/lib/environment.dart
+++ wefood/lib/environment.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/foundation.dart';
+
 class Environment {
-  static const String serverUrl = 'https://api.wefoodcompany.com';
+  static const String serverUrl = kDebugMode  ? 'https://dev.api.wefoodcompany.com' : 'https://api.wefoodcompany.com';
   static const String apiUrl = '$serverUrl/api/auth/';
   static const String storageUrl = '$serverUrl/storage/';
 
@@ -10,10 +12,13 @@ class Environment {
   static const String supportPhone = '+51912051667';
   static const String supportEmail = 'info@wefoodcompany.com';
 
-  static const bool openpayIsSandbox = false;
-  static const String openpayMerchantId = 'mg1ippvpuekjrkszeuxc'; // Sandbox -> 'muorexrewu3587xbmooq'; Production -> 'mg1ippvpuekjrkszeuxc'
-  static const String openpayPublicKey = 'pk_540273ba143943b999db84f432e85aa3'; // Sandbox -> 'pk_d0651ad6528145c69c79de9002864eac'; Production -> 'pk_540273ba143943b999db84f432e85aa3'
-  static const String openpaySecretKey = 'sk_da74b3a391734614a0ab59eae4c3cb9c'; // Sandbox -> 'sk_11db13e676de4739ba6c728fbba3efe0'; Production -> 'sk_da74b3a391734614a0ab59eae4c3cb9c'
+  static const bool openpayIsSandbox = kDebugMode;
+  static const String openpayMerchantId = kDebugMode  ? 'mg1ippvpuekjrkszeuxc' : 'mg1ippvpuekjrkszeuxc'; 
+  // Sandbox -> 'muorexrewu3587xbmooq'; Production -> 'mg1ippvpuekjrkszeuxc'
+  static const String openpayPublicKey = kDebugMode  ? 'pk_540273ba143943b999db84f432e85aa3' : 'pk_540273ba143943b999db84f432e85aa3'; 
+  // Sandbox -> 'pk_d0651ad6528145c69c79de9002864eac'; Production -> 'pk_540273ba143943b999db84f432e85aa3'
+  static const String openpaySecretKey = kDebugMode  ?  'sk_da74b3a391734614a0ab59eae4c3cb9c' : 'sk_da74b3a391734614a0ab59eae4c3cb9c'; 
+  // Sandbox -> 'sk_11db13e676de4739ba6c728fbba3efe0'; Production -> 'sk_da74b3a391734614a0ab59eae4c3cb9c'
   static const String openpayApiUrl = 'https://${Environment.openpayIsSandbox ? 'sandbox-' : ''}api.openpay.pe/v1/${Environment.openpayMerchantId}/';
 
   static const double minimumPrice = 5;
